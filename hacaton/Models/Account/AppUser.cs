fb7302da5bc2de6454e1b00812450f8c16850b64
Jeyhun-Aghayev <bd8ez2q6v@code.edu.az>
2024-12-06 13:13:05 +0400

    sdasd

--- hacaton/Models/Account/AppUser.cs
+++ hacaton/Models/Account/AppUser.cs
@@ -7,5 +7,7 @@ namespace hacaton.Models.Account
 		public string Name { get; set; }
 		public string Surname { get; set; }
 		public bool RememberMe { get; set; }
+		public int DepartmentId { get; set; }
+		public Department Department { get; set; }
 	}
 }
