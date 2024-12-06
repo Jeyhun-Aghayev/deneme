44d479adaa65a01f3ef61e225fb3339b8310136c
Jeyhun-Aghayev <bd8ez2q6v@code.edu.az>
2024-12-06 12:56:10 +0400

    asd

--- hacaton/Areas/Manage/Controllers/EmployeeManagementController.cs
+++ hacaton/Areas/Manage/Controllers/EmployeeManagementController.cs
@@ -1,16 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using hacaton.Helpers;
+using hacaton.Models.Account;
+using hacaton.ViewModels.Account;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace hacaton.Areas.Manage.Controllers
 {
 	public class EmployeeManagementController : Controller
 	{
+		private readonly UserManager<AppUser> _userManager;
+		private readonly SignInManager<AppUser> _signInManager;
+		private readonly RoleManager<IdentityRole> _roleManager;
+
+		public EmployeeManagementController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<IdentityRole> roleManager)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+			_roleManager = roleManager;
+		}
 		public IActionResult Index()
 		{
 			return View();
 		}
-		public IActionResult Update()
-		{
+		//public IActionResult Update(){ }
+		//public IActionResult Delete() { }
 
+		[HttpPost]
+		public async Task<IActionResult> Create(RegisterVm registerVm)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View();
+			}
+			AppUser user = new AppUser()
+			{
+				Name = registerVm.Name,
+				Email = registerVm.Email,
+				Surname = registerVm.Surname,
+				UserName = registerVm.Username,
+			};
+			var result = await _userManager.CreateAsync(user, registerVm.Password);
+			if (!result.Succeeded)
+			{
+				foreach (var error in result.Errors)
+				{
+					ModelState.AddModelError("", error.Description);
+				}
+				return View();
+			}
+
+			await _userManager.AddToRoleAsync(user, UserRole.Member.ToString());
+			return RedirectToAction(nameof(Index), "home");
 		}
 	}
 }
